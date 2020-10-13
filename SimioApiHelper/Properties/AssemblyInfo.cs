004cfcb9746133c0642359f68bde7e46322b9e84
Dan Houck <dan.houck@batpox.com>
2020-10-13 08:14:57 -0400

    Version update

--- SimioApiHelper/Properties/AssemblyInfo.cs
+++ SimioApiHelper/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("20.8.23.0")]
-[assembly: AssemblyFileVersion("20.8.23.0")]
+[assembly: AssemblyVersion("20.10.11.0")]
+[assembly: AssemblyFileVersion("20.10.11.0")]
